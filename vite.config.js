de549b15d50cf5b788794d86dff5926acff74877
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-15 20:49:37 +0200

    actualizando

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "127.0.0.1",
@@ -12,7 +11,6 @@ export default defineConfig({
       protocol: "ws"
     }
   },
-  
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
