1f0713d99f3f923795b0cade62afaf1651fd1168
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-15 21:09:32 +0200

    volviendo a la version de antes

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,11 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "127.0.0.1",
-    port: 5173,
-    hmr: {
-      host: "localhost",
-      protocol: "ws"
-    }
+    port: 5173
   },
-  
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
